09f5d5c385c6d4fd1bd76accdb042a04f2b2d6ef
Mike Marseglia <mike@marseglia.org>
2016-06-15 09:06:04 -0400

    update spec test, add packer_version as puppet fact.

--- spec/classes/init_spec.rb
+++ spec/classes/init_spec.rb
@@ -1,16 +1,23 @@
 require 'spec_helper'
 
 describe 'packer' do
-	let(:facts) { {:architecture => 'x86_64', :kernel => 'Linux', :puppetversion => '3.6.2 (Puppet Enterprise 3.3.2)', :osfamily => 'RedHat' } }
+	context 'with default parameters on Linux 64-bit' do
+	let(:facts) { {
+		:architecture => 'x86_64',
+		:kernel => 'Linux',
+		:packer_version => '0.10.0',
+	} }
 
 	# required for module archive
 	let(:environment) { 'production' }
 
 	it { should compile }
 	it { should contain_class('packer') }
+
 	it do
 	  is_expected.to contain_archive('/tmp/packer_0.10.1_linux_amd64.zip').with({
 	    'source' => 'https://releases.hashicorp.com/packer/0.10.1/packer_0.10.1_linux_amd64.zip',
 	  })
+	end
 	end
 end

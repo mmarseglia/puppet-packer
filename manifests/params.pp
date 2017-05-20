dd19d1fee2d77791acc282a3f7104e40f3315518
Mike Marseglia <mike@marseglia.org>
2017-05-20 12:20:01 -0400

    Bump packer version to 1.0.0

--- manifests/params.pp
+++ manifests/params.pp
@@ -11,7 +11,7 @@ class packer::params {
 
   $base_url  = 'https://releases.hashicorp.com/packer'
   $ensure    = 'installed'
-  $version   = '0.10.1'
+  $version   = '1.0.0'
   $proxy     = undef
 
   case downcase($::kernel) {

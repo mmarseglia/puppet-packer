85a9608fca363d43a8f98ccb6f7f0ccfc8780105
Mike Marseglia <mike@marseglia.org>
2019-03-11 15:32:55 -0400

    update tests to cover all supported operating systems identified in module metadata. removed darwin support.

--- spec/classes/init_spec.rb
+++ spec/classes/init_spec.rb
@@ -1,19 +1,12 @@
 require 'spec_helper'
 
 describe 'packer' do
-  context 'with default parameters on Linux 64-bit' do
-    let(:facts) do
-      {
-        architecture: 'x86_64',
-        kernel: 'Linux',
-      }
+  on_supported_os.each do |os, facts|
+    context "with default parameters on #{os}" do
+      let (:facts) { facts }
+      it { is_expected.to compile }
+      it { is_expected.to contain_class('packer') }
+      it { is_expected.to contain_archive('/tmp/packer_1.1.2_linux_amd64.zip').with('source' => 'https://releases.hashicorp.com/packer/1.1.2/packer_1.1.2_linux_amd64.zip') }
     end
-
-    # required for module archive
-    let(:environment) { 'production' }
-
-    it { is_expected.to compile }
-    it { is_expected.to contain_class('packer') }
-    it { is_expected.to contain_archive('/tmp/packer_1.1.2_linux_amd64.zip').with('source' => 'https://releases.hashicorp.com/packer/1.1.2/packer_1.1.2_linux_amd64.zip') }
   end
 end

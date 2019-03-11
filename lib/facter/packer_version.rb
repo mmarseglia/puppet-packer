d08ac481a0c272a10c8138d8fa5e941d79119bd8
Mike Marseglia <mike@marseglia.org>
2019-03-11 13:24:17 -0400

    misc rubocop fixes

--- lib/facter/packer_version.rb
+++ lib/facter/packer_version.rb
@@ -6,17 +6,19 @@
 # currently installed.
 #
 #
-Facter.add("packer_version") do
+Facter.add('packer_version') do
   setcode do
     begin
       kernel = Facter.value('kernel').downcase
-        case kernel
-        when /linux|darwin/
-          version = Facter::Util::Resolution.exec('/usr/local/bin/packer version')
-        when 'windows'
-          version = Facter::Util::Resolution.exec('C:\Windows\system32\packer version')
-        end
-        /\d.*$/.match(version)
+
+      case kernel
+      when %r{linux|darwin}
+        version = Facter::Util::Resolution.exec('/usr/local/bin/packer version')
+      when 'windows'
+        version = Facter::Util::Resolution.exec('C:\Windows\system32\packer version')
+      end
+
+      %r{\d.*$}.match(version)
     rescue
       nil
     end
--- spec/defines/plugin_spec.rb
+++ spec/defines/plugin_spec.rb
@@ -1,15 +1,16 @@
 require 'spec_helper'
 
 describe 'packer::plugin' do
-				let(:facts) { {:architecture => 'x86_64', :kernel => 'Linux', :puppetversion => '3.6.2 (Puppet Enterprise 3.3.2)', :osfamily => 'RedHat' } }
+  let(:facts) { { architecture: 'x86_64', kernel: 'Linux', puppetversion: '3.6.2 (Puppet Enterprise 3.3.2)', osfamily: 'RedHat' } }
 
-				# required for module archive
-				let(:environment) { 'production' }
+  # required for module archive
+  let(:environment) { 'production' }
 
-				let(:params) { { :version	=> 'v.1.2.3' } }
+  let(:params) { { version: 'v.1.2.3' } }
 
-				context 'with defaults for all parameters' do
-								let(:title) { 'myplug' }
-								it { should compile }
-				end
+  context 'with defaults for all parameters' do
+    let(:title) { 'myplug' }
+
+    it { is_expected.to compile }
+  end
 end

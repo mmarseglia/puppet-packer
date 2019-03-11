a0ae73a1669e84cf34a0bd10408542168caad1c2
Mike Marseglia <mike@marseglia.org>
2019-03-11 15:51:55 -0400

    fix warning: rubocop: spec/classes/init_spec.rb:6:10: Lint/ParenthesesAsGroupedExpression:  interpreted as grouped expression.

--- spec/classes/init_spec.rb
+++ spec/classes/init_spec.rb
@@ -3,7 +3,8 @@ require 'spec_helper'
 describe 'packer' do
   on_supported_os.each do |os, facts|
     context "with default parameters on #{os}" do
-      let (:facts) { facts }
+      let(:facts) { facts }
+
       it { is_expected.to compile }
       it { is_expected.to contain_class('packer') }
       it { is_expected.to contain_archive('/tmp/packer_1.1.2_linux_amd64.zip').with('source' => 'https://releases.hashicorp.com/packer/1.1.2/packer_1.1.2_linux_amd64.zip') }

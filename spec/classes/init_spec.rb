e1840d909530b454fd4ab7916f08b6743f7e5fbe
Mike Marseglia <mike@marseglia.org>
2019-03-11 13:44:16 -0400

    fix syntax error in unit test

--- spec/classes/init_spec.rb
+++ spec/classes/init_spec.rb
@@ -14,7 +14,6 @@ describe 'packer' do
 
     it { is_expected.to compile }
     it { is_expected.to contain_class('packer') }
-
-    it is_expected.to contain_archive('/tmp/packer_1.1.2_linux_amd64.zip').with('source' => 'https://releases.hashicorp.com/packer/1.1.2/packer_1.1.2_linux_amd64.zip')
+    it { is_expected.to contain_archive('/tmp/packer_1.1.2_linux_amd64.zip').with('source' => 'https://releases.hashicorp.com/packer/1.1.2/packer_1.1.2_linux_amd64.zip') }
   end
 end
